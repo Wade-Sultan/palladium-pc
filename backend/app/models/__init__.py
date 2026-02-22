5f738cfbef14caeb85b2bf10c77bc3b6e1c9eb31
Wade Sultan <wadesultan@gmail.com>
2026-02-21 20:54:36 -0800

    Added software and games catalogs to init

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -5,3 +5,5 @@ from .pcparts import PCPart
 from .pcbuild import PCBuild, BuildPart
 from .listing import Listing
 from .benchmarks import BenchmarkType, CPUBenchmarkScores, GPUBenchmarkScores
+from .software_catalog import Software, SoftwareCategory, SoftwareMinimumPart
+from .games_catalog import Game, GameMinimumPart
