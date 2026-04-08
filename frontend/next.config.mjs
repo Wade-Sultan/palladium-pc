1d4942c7516d7d7aeca0aec73bbe7f2202b0b660
Wade Sultan <wadesultan@gmail.com>
2026-04-08 00:12:12 -0700

    debugging frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,7 +6,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 /** @type {import('next').NextConfig} */
 const NextConfig = {
     distDir: 'dist',
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     turbopack: {
         root: resolve(__dirname, '..'),
     },
