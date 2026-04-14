d8d08d3b6481a51dfbf7883f0f0e92eb0bce38ca
Wade Sultan <wadesultan@gmail.com>
2026-04-13 23:57:48 -0700

    Removed colocated hooks

--- admin/assets/js/app.js
+++ admin/assets/js/app.js
@@ -22,7 +22,6 @@ import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
-import {hooks as colocatedHooks} from "phoenix-colocated/admin"
 import topbar from "../vendor/topbar"
 import daisyui from "../vendor/daisyui"
 import duitheme from "../vendor/daisyui-theme"
@@ -32,7 +31,7 @@ const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute
 const liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   params: {_csrf_token: csrfToken},
-  hooks: {...colocatedHooks},
+  hooks: {},
 })
 
 // Show progress bar on live navigation and form submits
